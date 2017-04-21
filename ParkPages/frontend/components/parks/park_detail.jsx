5b44256ddcf1a27ae3d14dc7ea2581ca9ea97bae
Philip Park <park.j.philip@gmail.com>
2017-04-21 08:05:59 -0400

    Fixed style and continue working on parkdetail component. pre-heroku push

--- ParkPages/frontend/components/parks/park_detail.jsx
+++ ParkPages/frontend/components/parks/park_detail.jsx
@@ -4,12 +4,18 @@ import { Link, withRouter } from 'react-router';
 class ParkDetail extends React.Component {
 	constructor(props) {
 		super(props);
-
 	}
 
   componentWillMount() {
-    this.props.fetchPark(this.props.parkId)
+    this.props.fetchPark(this.props.parkId);
   }
+
+  componentWillReceiveProps(newProps) {
+  if (this.props.params.id !== newProps.params.id) {
+    this.props.fetchPark(newProps.params.id);
+    }
+  }
+
 	render() {
 		return (
       <h1>we here </h1>
