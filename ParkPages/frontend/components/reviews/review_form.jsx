bd7eb4cd5646d194a32a09c3957b7f1a1516b73c
Philip Park <park.j.philip@gmail.com>
2017-04-25 08:28:21 -0400

    inital commit 4/25

--- ParkPages/frontend/components/reviews/review_form.jsx
+++ ParkPages/frontend/components/reviews/review_form.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Link, withRouter } from 'react-router';
 // import ParkIndexItem from './park_index_item';
 
+//need to pass down current user and check if they are logged in
+
 class ReviewForm extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
   render() {
-    return <div> Form Here </div>;
+    return (
+      <form>
+        <div className="top-review-message"> Your review </div>
+        <div> star input here </div>
+        <textarea />
+      </form>
+    );
 	}
 }
 
