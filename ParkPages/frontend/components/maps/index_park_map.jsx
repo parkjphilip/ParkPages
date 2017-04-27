972071b7c09ad01cf4890bc0fd2d006d43b5b750
Philip Park <park.j.philip@gmail.com>
2017-04-27 17:51:43 -0400

    fixed updating map on new searches

--- ParkPages/frontend/components/maps/index_park_map.jsx
+++ ParkPages/frontend/components/maps/index_park_map.jsx
@@ -4,6 +4,27 @@ import { withRouter } from 'react-router';
 
 class IndexParkMap extends React.Component {
 
+  componentWillReceiveProps(newProps) {
+    debugger
+
+    if (this.props.parks != newProps.parks ) {
+
+      var myCenter = { lat: newProps.parks[0].lat, lng: newProps.parks[0].lng };
+
+      const mapOptions = {
+        center: myCenter,
+        zoom: 10
+      };
+      this.map = new google.maps.Map(this.mapNode, mapOptions);
+      newProps.parks.forEach(park => {
+        var marker = new google.maps.Marker({
+          position: { lat: park.lat, lng: park.lng },
+          map: this.map
+        });
+      });
+    }
+  }
+
   componentDidMount() {
     var myCenter = { lat: this.props.parks[0].lat, lng: this.props.parks[0].lng };
 
--- ParkPages/frontend/components/maps/park_map.jsx
+++ ParkPages/frontend/components/maps/park_map.jsx
@@ -5,13 +5,11 @@ import { withRouter } from 'react-router';
 class ParkMap extends React.Component {
 
   componentDidMount() {
-
     var myCenter = { lat: this.props.currentPark.lat, lng: this.props.currentPark.lng };
     const mapOptions = {
       center: myCenter,
       zoom: 13
     };
-
     this.map = new google.maps.Map(this.mapNode, mapOptions);
     var marker = new google.maps.Marker({
       position: myCenter,
