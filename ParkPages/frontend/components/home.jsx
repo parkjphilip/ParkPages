6a205fa04902f595cc3fbc9f427cfb7b5e629b02
Philip Park <park.j.philip@gmail.com>
2017-04-27 11:57:54 -0400

    added working category buttons on the home page

--- ParkPages/frontend/components/home.jsx
+++ ParkPages/frontend/components/home.jsx
@@ -5,9 +5,26 @@ import SearchContainer from './search/search_container';
 class Home extends React.Component {
 	render() {
 		return (
+      <div>
         <div className="image-box">
           <img className="home-logo" src="/images/ParkPages.png" alt="Logo" />
         </div>
+        <div className="tag-category-buttons">
+          <button className="tag-category-button" onClick={()=>{this.props.router.push("parks?query=Playground");}}>
+            Playground
+          </button>
+          <button className="tag-category-button" onClick={()=>{this.props.router.push("parks?query=Basketball");}}>
+            Basketball
+          </button>
+          <button className="tag-category-button" onClick={()=>{this.props.router.push("parks?query=Relaxing");}}>
+            Relaxing
+          </button>
+          <button className="tag-category-button" onClick={()=>{this.props.router.push("parks?query=Barbecue");}}>
+            Barbecue
+          </button>
+        </div>
+
+      </div>
 		);
 	}
 }
